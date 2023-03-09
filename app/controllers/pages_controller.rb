4b9a6a50be037e2fa08d2cf5ddf7aba6b9d84c15
Edin Tuna <ed.tun@outlook.com>
2023-03-09 17:30:54 +0100

    updated navbar bottom + qr code confirmation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    unless current_user
+      redirect_to new_user_session_path
+    end
   end
 
   def qr_code_show
