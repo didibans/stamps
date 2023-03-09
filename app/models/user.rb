00b525ea7d851c1d29a3e4947f3f12f2c3937ff2
Edin Tuna <ed.tun@outlook.com>
2023-03-09 12:11:28 +0100

    changed map style + added style to qr code + added qr code path

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
 
   def generate_qrcode
     # Get the host
-    host = Rails.application.routes.default_url_options[:host]
+    # host = Rails.application.routes.default_url_options[:host]
     # host = Rails.application.config.action_controller.default_url_options[:host]
 
     # Create the QR code object
@@ -35,7 +35,7 @@ class User < ApplicationRecord
     # Create a new PNG object
     png = qrcode.as_png(
       bit_depth: 1,
-      border_modules: 4,
+      border_modules: 1,
       color_mode: ChunkyPNG::COLOR_GRAYSCALE,
       color: "black",
       file: nil,
@@ -43,7 +43,7 @@ class User < ApplicationRecord
       module_px_size: 6,
       resize_exactly_to: false,
       resize_gte_to: false,
-      size: 120
+      size: 250
     )
 
     # Attach the QR code to the active storage
