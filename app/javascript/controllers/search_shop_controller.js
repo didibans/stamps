7ac7f60c8dc1f7ab63e6365419ef6f5d44d7bc2d
Julian Schopen <j-schopen@web.de>
2023-03-09 16:07:56 +0100

    fixing problem with empty searchbar

--- app/javascript/controllers/search_shop_controller.js
+++ app/javascript/controllers/search_shop_controller.js
@@ -5,12 +5,13 @@ export default class extends Controller {
   static targets = ["form", "input", "list"]
 
   update() {
-    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
-    fetch(url, {headers: {"Accept": "text/plain"}})
+    if (!this.inputTarget.value.length == 0) {
+      const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+      fetch(url, {headers: {"Accept": "text/plain"}})
       .then(response => response.text())
       .then((data) => {
         this.listTarget.innerHTML = data;
       })
-
+    }
   }
 }
